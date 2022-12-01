d02bd7ce6c16a0b291778f54dfecd4bf54af2fc2
Asylniet <zasylniet@mail.ru>
2022-12-01 13:54:28 +0600

    started main page

--- js/app.js
+++ js/app.js
@@ -1,19 +1,31 @@
+function toggleSelectable(selectable) {
+  if (selectable.classList.contains('open')) {
+    selectable.classList.remove('open');
+  } else {
+    selectable.classList.add('open');
+  }
+}
+
 const selectables = document.querySelectorAll('.selectable');
-const languageContainer = document.querySelector('.language-options');
 const optionsContainer = document.querySelector('.options');
 selectables.forEach((selectable) => {
   const selected = selectable.querySelector('.selected');
   selected.addEventListener('click', () => {
-    if (selectable.classList.contains('open')) {
-      selectable.classList.remove('open');
-    } else {
-      selectable.classList.add('open');
-    }
+    toggleSelectable(selectable);
   });
-})
 
+  // Close selectable dropdown if clicked outside of selectable element
+  document.addEventListener('click', function(event) {
+    if(!selectable.contains(event.target)) {
+      selectable.classList.remove('open');
+    };
+  });
+});
+
+const languageContainer = document.querySelector('.language-options');
 let languageOptions = optionsContainer.querySelectorAll(".language-option");
 languageOptions.forEach((option) => {
+  const selected = languageContainer.querySelector('.selected');
   option.addEventListener("click", () => {
     const language = option.textContent;
     languageContainer.setAttribute('selected', language);
